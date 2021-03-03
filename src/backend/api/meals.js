69e121d9ff5637b5841749687f863566a44fd905
Enqira <moenkira@yahoo.com>
2021-03-03 04:21:14 +0100

    Working on meals.js

--- src/backend/api/meals.js
+++ src/backend/api/meals.js
@@ -1,15 +1,37 @@
-const express = require("express");
-const router = express.Router();
-const knex = require("../database");
+const express = require("express")
+const router = express.Router()
+const knex = require("../database")
 
-router.get("/", async (request, response) => {
+router.get("/", async (req, res) => {
   try {
-    // knex syntax for selecting things. Look up the documentation for knex for further info
-    const titles = await knex("meals").select("title");
-    response.json(titles);
+    if (req.query.id) {
+      const meal = await knex("meals").where({ id: req.query.id })
+      res.send(meal)
+      return
+    }
+    // knex syntax for selecting things.
+    const meals = await knex("meals").select("title")
+    console.log(meals)
+    res.json(meals)
   } catch (error) {
-    throw error;
+    throw error
   }
-});
+})
+router.post("/", async (req, res) => {
+  try {
+    console.log("post meals")
+    const addMeal = await knex("meals").insert({
+      title: req.body.title,
+      description: req.body.description,
+      location: req.body.location,
+      when: req.body.when,
+      max_reservations: req.body.max_reservations,
+      price: req.body.price
+    })
+    res.send("Done")
+  } catch (error) {
+    throw error
+  }
+})
 
-module.exports = router;
+module.exports = router
